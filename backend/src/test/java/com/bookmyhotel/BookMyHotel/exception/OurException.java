8b0b2553a41923795fb7b632975064b80b00cfc6
Sharmaji0108 <anujsh0108@gmail.com>
2025-07-20 10:47:17 +0530

    Delete backend directory

--- backend/src/test/java/com/bookmyhotel/BookMyHotel/exception/OurException.java
+++
@@ -1,9 +0,0 @@
-package com.bookmyhotel.BookMyHotel.exception;
-
-@SuppressWarnings("serial")		/*Remove*/
-public class OurException extends RuntimeException {
-
-    public OurException(String message) {
-        super(message);
-    }
-}
